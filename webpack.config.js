b7509a7d7d55fe0f0086e4a741141676674eaad4
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-07-19 23:32:49 -0300

    loading favicon

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
+      favicon: path.resolve(__dirname, 'src', 'assets', 'scissors.svg'),
     }),
   ],
 };
