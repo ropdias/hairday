f1e2fae826eb6b5d0f4924ab940b3eeaa0f71160
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-07-25 00:25:13 -0300

    sending a selected hour to the API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 
 const form = document.querySelector('form');
+const clientName = document.getElementById('client');
 const selectedDate = document.getElementById('date');
 
 // Current date to format input
@@ -12,4 +13,33 @@ selectedDate.min = inputToday;
 
 form.onsubmit = (event) => {
   event.preventDefault();
+
+  try {
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert('Informe o nome do client!');
+    }
+
+    const hourSelected = document.querySelector('.hour-selected');
+
+    if (!hourSelected) {
+      return alert('Selecione a hora!');
+    }
+
+    const [hour] = hourSelected.innerText.split(':');
+
+    const when = dayjs(selectedDate.value).add(hour, 'hour');
+
+    const id = new Date().getTime();
+
+    console.log({
+      id,
+      name,
+      when,
+    });
+  } catch (error) {
+    alert('Não foi possível realizar o agendamento.');
+    console.log(error);
+  }
 };
