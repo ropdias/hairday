da23bbbd8c775183cb7b49a3fe237ff0c36a7fdf
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-07-26 21:58:51 -0300

    getting the schedules from the API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,10 +1,15 @@
 import { hoursLoad } from '../form/hours-load.js';
+import { scheduleFetchByDay } from '../../services/schedule-fetch-by-day.js';
 
 const selectedDate = document.getElementById('date');
 
-export function schedulesDay() {
+export async function schedulesDay() {
   // Getting input date
   const date = selectedDate.value;
+
+  // Getting data from the API
+  const dailySchedules = await scheduleFetchByDay({ date });
+  console.log(dailySchedules);
 
   // Rendering opening hours
   hoursLoad({ date });
